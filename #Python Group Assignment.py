9a05649c532e950d273563a55cf1c83f09a79078
Muhetojess <84878207+MuhetoJess@users.noreply.github.com>
2025-06-27 12:05:04 +0200

    Add files via upload

---
+++ #Python Group Assignment.py
@@ -0,0 +1,28 @@
+#Python Group Assignment
+
+#Answer to Question
+def check_palindrome(text):
+    if text == text[::-1]:
+        print("Yes, it is a palindrome")
+    else:
+        print("No, it is a palindrome")
+
+# Get User input
+User_text = input("Enter a string to check if it's a palindrome")
+check_palindrome(User_text)
+
+
+#Answer to Question3
+def combine_and_iterate(text1, text2):
+    Combined = text1 + text2
+    Char_list = [char for char in Combined]
+    return Char_list
+
+# Get User Input
+text1 = input("Enter the first text: ")
+text2 = input("Enter the second text: ")
+
+# process and display result
+result = combine_and_iterate(text1, text2)
+print("list of characters from combined text: ", result)
+print("Thank you for using my application \n after processing the Input.") 
